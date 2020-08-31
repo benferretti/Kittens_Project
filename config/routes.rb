035c8ccc4dee18792e2bc16daf0226b605c265af
Lamalle <julienlamalle63100@gmail.com>
2020-08-31 21:50:53 +0200

    User show added and avatar edit available

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   
   root 'products#index'
+  
+  devise_for :users
 
   resources :kituis, only: [:index]
   resources :products, only: [:show, :index]
-  devise_for :users
+  resources :users, only: [:show]
 end
