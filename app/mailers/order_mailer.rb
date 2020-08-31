55c3f61bfba6577b321f94f3f3d66688b9751da4
Ben <benjamin.valadilaine@gmail.com>
2020-08-31 15:55:31 +0200

    OrderMailer done, missing array of products

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -2,18 +2,12 @@ class OrderMailer < ApplicationMailer
   default from: 'amaury.desbazeille@gmail.com'
 
   def order_confirmed(order)
-    @user = order.user_id
-    puts '$$$$$$$$$$$$$$$$ mailer $$$$$$$$$$$$$$$$$$'
-    puts @user
-    puts order.id
-    
-    #create an array with all products ordered
-    #@products_ordered = Array.new
-    #Product_orders.find(order.id).each do |item|
-    #  @products_ordered << Product.find(item.id)
-    #end
-
+    @user = User.find(order.user_id)
     @url  = 'https://kittens-project-develop.herokuapp.com/'
+
+    # TO DO : create an array with all products ordered
+    @products_ordered = Array.new
+    ProductOrder.find_by(order_id: order.id)
 
     mail(to: @user.email, subject: 'Confirmation de votre commande')
   end
