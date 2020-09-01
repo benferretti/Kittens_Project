2870185aa202328679b6b8653b5c5ff14f2f1780
AmauryDBZ <amaury.desbazeille@gmail.com>
2020-09-01 10:19:14 +0200

    sendgrid mailer pluged in local

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,7 +9,7 @@ Category.reset_pk_sequence
 
 Faker::Config.locale = 'fr'
 
-user_email_array = ['aomee@gmail.com', 'julien@gmail.com', 'benjamin@gmail.com' , 'amaury@gmail.com' , 'benoit@gmail.com']
+user_email_array = ['aomee@yopmail.com', 'julien@yopmail.com', 'benjamin@yopmail.com' , 'amaury@yopmail.com' , 'benoit@yopmail.com']
 category_array = ['Imparisyllabique' , 'Hétéroclite' , 'Rocambolesque' , 'Délectable' , 'Cataclysmique']
 product_name_array = ['Anakin','Bellatrix','Excalibur','Galilée','Gertrude','Itachi','Ondine','Padme','Perceval','Pétunia','Pikachu','Platon','Princesse Leia','Raspoutine','Rodolphe','Roi Arthur','Sauron','Shrek','Vador','Yoda']
 product_description_array = ["Obtenez l'élu de la force, il vous le rendra bien (nous ne répondrons pas de son comportement).","Tu ne seras jamais de taille à rivaliser avec moi, pauvre petit bonhomme...","Seul l'élu pourra me posséder.","Je n'ai jamais rencontré d'homme si ignorant qu'il n'eut quelque chose à m'apprendre. Achète-moi.","Comme son nom ne l'indique pas…","Quand tu posséderas la même pupille que moi… viens à moi !","J'adore l'eau, dans 20/30 ans il y en aura plus.","Ainsi s'éteint la liberté. Sous des applaudissements.","C'est pas faux.","Elle fait un peu la star mais elle est cool.","Attrapez les tous.","Les hommes ne veulent pas ce qu'ils font, mais ce en vue de quoi ils font ce qu'ils font.","Obi-wan, vous êtes mon dernier espoir.","Je me battrai pour toi… Tant que tu me paieras.","Craquez pour ce petit geek.","Il commence à doucement me faire chier celui là aussi!","Un Anneau pour les gouverner tous, un Anneau pour les amener tous et dans les ténèbres les lier.","J’te double, et je suis même pas à fond…","Rejoignez le côté obscur de la force et controllez la galaxie aux côtés de Vador le chat. Fourni avec la tasse.","M'acheter tu feras, satisfaction tu obtiendras, jeune padawan."]
@@ -24,14 +24,14 @@ i = 0
   puts "Seeding User #{user.email}"
 end
 i = 0
-5.times do 
+5.times do
   category = Category.create(
     name: category_array[i]
   )
   i += 1
 end
-i = 0 
-20.times do 
+i = 0
+20.times do
   product = Product.create(
     name: product_name_array[i],
     description: product_description_array[i],
@@ -41,5 +41,3 @@ i = 0
   )
   i += 1
 end
-
-
