a7b406c8914c4042c27580ca79a2912229a70490
AmauryDBZ <amaury.desbazeille@gmail.com>
2020-09-02 10:18:45 +0200

    link added in mailers

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,5 @@
 class ApplicationMailer < ActionMailer::Base
   default from: 'amaury.desbazeille@gmail.com'
   layout 'mailer'
+  @url  = 'https://kittens-project-develop.herokuapp.com/'
 end
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,7 +3,7 @@ class UserMailer < ApplicationMailer
 
   def send_new_user_message(user)
     @user = user
-
+    @url  = 'https://kittens-project-develop.herokuapp.com/'
     mail(to: @user.email, subject: "Confirmation d'inscription")
   end
 end
